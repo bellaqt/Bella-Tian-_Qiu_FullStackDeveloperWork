b3a739e15341cee84afb76ceeb2657275f45ed51
bellaqt <chang20204@gmail.com>
2025-11-22 21:00:28 +0800

    6

--- Bella(Tian)_Qiu_FullStackDeveloperWork/Program.cs
+++ Bella(Tian)_Qiu_FullStackDeveloperWork/Program.cs
@@ -28,6 +28,14 @@ app.UseCors("AllowFrontend");
 // app.UseHttpsRedirection();
 app.UseAuthorization();
 
+app.Use(async (context, next) =>
+{
+    context.Response.Headers.Add("Access-Control-Allow-Origin", "*");
+    context.Response.Headers.Add("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    context.Response.Headers.Add("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    await next();
+});
+
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapControllers();

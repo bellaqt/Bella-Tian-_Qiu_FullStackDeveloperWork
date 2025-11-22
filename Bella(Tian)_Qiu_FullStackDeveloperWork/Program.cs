20c6c31f043201692fcc8adcc5a0712b84ae903b
bellaqt <chang20204@gmail.com>
2025-11-22 20:38:55 +0800

    5

--- Bella(Tian)_Qiu_FullStackDeveloperWork/Program.cs
+++ Bella(Tian)_Qiu_FullStackDeveloperWork/Program.cs
@@ -1,55 +1,40 @@
-using Bella_Tian__Qiu_FullStackDeveloperWork.Services;
 using Amazon.S3;
-using Amazon.S3.Model;
+using Bella_Tian__Qiu_FullStackDeveloperWork.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
     {
-        policy.WithOrigins("http://13.236.207.55:5173")
-              .AllowAnyHeader()
-              .AllowAnyMethod()
-              .AllowCredentials(); 
+        policy.WithOrigins(
+            "http://13.236.207.55:5173",
+            "http://localhost:5173"
+        )
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowCredentials();
     });
 });
 
 builder.Services.AddSingleton<CarsStore>();
 builder.Services.AddHostedService<RegistrationCheckService>();
 builder.Services.AddSignalR();
-
-var awsSection = builder.Configuration.GetSection("AWS");
-var region = awsSection["Region"];
-
-builder.Services.AddDefaultAWSOptions(builder.Configuration.GetAWSOptions());
 builder.Services.AddAWSService<IAmazonS3>();
 
-var app = builder.Build(); 
-
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+var app = builder.Build();
 
 app.UseCors();
-
-//app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
 app.MapControllers();
 app.MapHub<CarStatusHub>("/carStatusHub");
+app.MapMethods("{*path}", new[] { "OPTIONS" }, (context) =>
+{
+    context.Response.StatusCode = 200;
+    return Task.CompletedTask;
+});
 
 app.Run("http://0.0.0.0:5000");
-Console.WriteLine("AWS Region = " + builder.Configuration["AWS:Region"]);

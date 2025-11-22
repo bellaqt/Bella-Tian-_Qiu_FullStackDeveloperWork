8f18aabc9006327a9ea394b36539ff2a9b46829a
bellaqt <chang20204@gmail.com>
2025-11-22 20:44:36 +0800

    6

--- Bella(Tian)_Qiu_FullStackDeveloperWork/Program.cs
+++ Bella(Tian)_Qiu_FullStackDeveloperWork/Program.cs
@@ -1,13 +1,16 @@
+﻿using Bella_Tian__Qiu_FullStackDeveloperWork.Services;
 using Amazon.S3;
-using Bella_Tian__Qiu_FullStackDeveloperWork.Services;
+using Amazon.S3.Model;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddCors(options =>
 {
-    options.AddDefaultPolicy(policy =>
+    options.AddPolicy("AllowFrontend", policy =>
     {
         policy.WithOrigins(
             "http://13.236.207.55:5173",
@@ -26,15 +29,22 @@ builder.Services.AddAWSService<IAmazonS3>();
 
 var app = builder.Build();
 
-app.UseCors();
+app.UseCors("AllowFrontend");      
+// app.UseHttpsRedirection();     
 app.UseAuthorization();
 
 app.MapControllers();
 app.MapHub<CarStatusHub>("/carStatusHub");
-app.MapMethods("{*path}", new[] { "OPTIONS" }, (context) =>
+
+app.MapMethods("{*path}", new[] { "OPTIONS" }, (HttpContext context) =>
 {
     context.Response.StatusCode = 200;
+    context.Response.Headers.Add("Access-Control-Allow-Origin", "http://13.236.207.55:5173");
+    context.Response.Headers.Add("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+    context.Response.Headers.Add("Access-Control-Allow-Headers", "*");
     return Task.CompletedTask;
 });
 
 app.Run("http://0.0.0.0:5000");
+
+Console.WriteLine("✅ Server running at http://0.0.0.0:5000");
